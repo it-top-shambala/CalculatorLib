a32c866b8f04480f94cdc7882c9ca830e2cf8b52
Starinin Andrey <starininandrey@gmail.com>
2022-07-03 19:23:14 +0300

    Add Person

---
+++ CalculatorLib.Lib/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace CalculatorLib.Lib;
+
+public class Person
+{
+    private int _id;
+    public int Id
+    {
+        get => _id;
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException();
+            }
+
+            _id = value;
+        }
+    }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+
+    public string FullName => $"{LastName} {FirstName}";
+}
---
+++ CalculatorLib.Test/PersonTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using CalculatorLib.Lib;
+using Xunit;
+
+namespace CalculatorLib.Test;
+
+public class PersonTest
+{
+    private Person CreatePerson()
+    {
+        return new Person()
+        {
+            Id = 1,
+            FirstName = "Andrey",
+            LastName = "Starinin"
+        };
+    }
+    
+    private Person CreateBadPerson()
+    {
+        return new Person()
+        {
+            Id = -1,
+            FirstName = "Andrey",
+            LastName = "Starinin"
+        };
+    }
+    
+    [Fact]
+    public void Id_Test()
+    {
+        var expected = 1;
+        
+        var actual = new Person()
+        {
+            Id = 1
+        };
+        
+        Assert.Equal(expected, actual.Id);
+    }
+
+    [Fact]
+    public void Id_Exception_Test()
+    {
+        Assert.Throws<ArgumentException>(() => new Person { Id = -1 } );
+    }
+
+    [Fact]
+    public void FirstName_Test()
+    {
+        var expected = "Andrey";
+
+        var actual = new Person { FirstName = "Andrey"};
+        
+        Assert.Equal(expected, actual.FirstName);
+    }
+    
+    [Fact]
+    public void LastName_Test()
+    {
+        var expected = "Starinin";
+
+        var actual = new Person { LastName = "Starinin"};
+        
+        Assert.Equal(expected, actual.LastName);
+    }
+    
+    [Fact]
+    public void FullName_Test()
+    {
+        var expected = "Starinin Andrey";
+
+        var person = new Person
+        {
+            FirstName = "Andrey", 
+            LastName = "Starinin"
+        };
+        var actual = person.FullName;
+        
+        Assert.Equal(expected, actual);
+    }
+}
